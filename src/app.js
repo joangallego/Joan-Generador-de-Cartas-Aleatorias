aa302f753b05d2f3cc4b2566cee303d7ce05d531
joangallego <123939267+joangallego@users.noreply.github.com>
2023-06-07 11:38:48 +0000

    Joan

--- src/app.js
+++ src/app.js
@@ -7,5 +7,31 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  const palos = ["♦", "♥", "♠", "♣"];
+
+  let figuras = palos[randomNum(palos)];
+
+  function randomNum(array) {
+    return Math.floor(Math.random() * array.length);
+  }
+
+  document.querySelector("#number").innerHTML = numeros[randomNum(numeros)];
+  document.querySelectorAll("#palo")[0].innerHTML = figuras;
+  document.querySelectorAll("#palo")[1].innerHTML = figuras;
 };
